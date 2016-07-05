a0665cfb8e5cb76faf5ab0936059f785a884527f
mahmudzero <duelwith_mahmud@live.com>
2016-07-05 14:07:50 -0400

    Initial Commit

---
+++ SquareOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SquareOne
+//
+//  Created by Mahmud Assamaray on 7/5/16.
+//  Copyright © 2016 Mahmud Assamaray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SquareOne/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SquareOne
+//
+//  Created by Mahmud Assamaray on 7/5/16.
+//  Copyright © 2016 Mahmud Assamaray. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
